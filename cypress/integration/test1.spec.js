166829cae51d9e67f4904c2bab07d37b46a803c5
ankushkatiyar <ankushkatiyar@cedcoss.com>
2022-03-15 23:01:05 +0530

    usdhsd

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -5,8 +5,3 @@ describe('visit cypress', () => {
     cy.visit('https://www.cypress.io/');
   });
 });
-describe('visit cypress', () => {
-  it('visit url', () => {
-    cy.visit('https://www.cypress.io/');
-  });
-});
