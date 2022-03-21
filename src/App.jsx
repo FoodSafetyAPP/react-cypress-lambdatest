5ac2e34872cf0ad81bc8333c32f7362c4911f687
ankushkatiyar <ankushkatiyar@cedcoss.com>
2022-03-21 19:46:26 +0530

    testing app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logsso" />
-        <p>Hello world</p>
+        <p>Hello ld</p>
         <a
           className="App-link"
           href="https://reactjs.org"
