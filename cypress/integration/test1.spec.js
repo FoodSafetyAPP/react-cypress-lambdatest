6dffcd0c606adafb0d001a14f8e37352da4986fb
ankushkatiyar <ankushkatiyar@cedcoss.com>
2022-03-26 17:20:07 +0530

    dsds

---
+++ cypress/integration/test1.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('visit cypress', () => {
+  it('visit url', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
+describe('visit cypress', () => {
+  it('visit url', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
---
+++ cypress/integration/test3.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('visit cypress', () => {
+  it('visit url', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
+describe('visit cypress', () => {
+  it('visit url', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
+
+describe('visit cypress', () => {
+  it('visit url', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
+
+describe('visit cypress', () => {
+  it('visit url', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
