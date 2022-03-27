b414c5488281bad45b12a554b1a32dbcbb649049
ankushkatiyar <ankushkatiyar@cedcoss.com>
2022-03-27 13:26:37 +0530

    aaa

--- cypress/integration/test5.spec.js
+++ cypress/integration/test5.spec.js
@@ -8,6 +8,6 @@ describe('visit cypress', () => {
 
 describe('visit cypress', () => {
   it('visit url', () => {
-    cy.visit('https://google.com');
+    cy.visit('https://gkkoogle.com');
   });
 });
