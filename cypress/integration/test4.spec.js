0dae3f9fcb8a302f8f9ba87555b37cf967b3df1f
ankushkatiyar <ankushkatiyar@cedcoss.com>
2022-03-29 21:31:32 +0530

    mnmndn

--- cypress/integration/test4.spec.js
+++ cypress/integration/test4.spec.js
@@ -2,12 +2,12 @@
 
 describe('visit cypress', () => {
   it('visit url', () => {
-    cy.visit('https://google.com/');
+    cy.visit('http://localhost:3000');
   });
 });
 
 describe('visit cypress1', () => {
   it('visit url1', () => {
-    cy.visit('https://jjdsjd.com/');
+    cy.visit('http://localhost:3000');
   });
 });
